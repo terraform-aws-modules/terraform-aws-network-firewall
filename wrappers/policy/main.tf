d734cd71bcd822b7073cc26179dce0a9b70a0934
Bryant Biggs <bryantbiggs@gmail.com>
2024-03-06 14:32:20 -0500

    fix: Run pre-commit to fix lint checks

--- wrappers/policy/main.tf
+++ wrappers/policy/main.tf
@@ -3,10 +3,16 @@ module "wrapper" {
 
   for_each = var.items
 
+  attach_resource_policy             = try(each.value.attach_resource_policy, var.defaults.attach_resource_policy, false)
   create                             = try(each.value.create, var.defaults.create, true)
-  tags                               = try(each.value.tags, var.defaults.tags, {})
+  create_resource_policy             = try(each.value.create_resource_policy, var.defaults.create_resource_policy, false)
   description                        = try(each.value.description, var.defaults.description, null)
   encryption_configuration           = try(each.value.encryption_configuration, var.defaults.encryption_configuration, {})
+  name                               = try(each.value.name, var.defaults.name, "")
+  ram_resource_associations          = try(each.value.ram_resource_associations, var.defaults.ram_resource_associations, {})
+  resource_policy                    = try(each.value.resource_policy, var.defaults.resource_policy, "")
+  resource_policy_actions            = try(each.value.resource_policy_actions, var.defaults.resource_policy_actions, [])
+  resource_policy_principals         = try(each.value.resource_policy_principals, var.defaults.resource_policy_principals, [])
   stateful_default_actions           = try(each.value.stateful_default_actions, var.defaults.stateful_default_actions, [])
   stateful_engine_options            = try(each.value.stateful_engine_options, var.defaults.stateful_engine_options, {})
   stateful_rule_group_reference      = try(each.value.stateful_rule_group_reference, var.defaults.stateful_rule_group_reference, {})
@@ -14,11 +20,5 @@ module "wrapper" {
   stateless_default_actions          = try(each.value.stateless_default_actions, var.defaults.stateless_default_actions, ["aws:pass"])
   stateless_fragment_default_actions = try(each.value.stateless_fragment_default_actions, var.defaults.stateless_fragment_default_actions, ["aws:pass"])
   stateless_rule_group_reference     = try(each.value.stateless_rule_group_reference, var.defaults.stateless_rule_group_reference, {})
-  name                               = try(each.value.name, var.defaults.name, "")
-  create_resource_policy             = try(each.value.create_resource_policy, var.defaults.create_resource_policy, false)
-  resource_policy_actions            = try(each.value.resource_policy_actions, var.defaults.resource_policy_actions, [])
-  resource_policy_principals         = try(each.value.resource_policy_principals, var.defaults.resource_policy_principals, [])
-  attach_resource_policy             = try(each.value.attach_resource_policy, var.defaults.attach_resource_policy, false)
-  resource_policy                    = try(each.value.resource_policy, var.defaults.resource_policy, "")
-  ram_resource_associations          = try(each.value.ram_resource_associations, var.defaults.ram_resource_associations, {})
+  tags                               = try(each.value.tags, var.defaults.tags, {})
 }

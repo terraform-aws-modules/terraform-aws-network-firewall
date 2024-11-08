772b90ba16998930b27430c3037992ec34253ae9
Andy Bohne <andy.bohne@armis.com>
2024-11-08 13:55:20 -0500

    Add nullable to variables in submodules with defaults

--- modules/rule-group/variables.tf
+++ modules/rule-group/variables.tf
@@ -2,12 +2,14 @@ variable "create" {
   description = "Controls if Network Firewall resources should be created"
   type        = bool
   default     = true
+  nullable    = false
 }
 
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
   default     = {}
+  nullable    = false
 }
 
 ################################################################################
@@ -18,6 +20,7 @@ variable "capacity" {
   description = "The maximum number of operating resources that this rule group can use. For a stateless rule group, the capacity required is the sum of the capacity requirements of the individual rules. For a stateful rule group, the minimum capacity required is the number of individual rules"
   type        = number
   default     = 100
+  nullable    = false
 }
 
 variable "description" {
@@ -30,18 +33,21 @@ variable "encryption_configuration" {
   description = "KMS encryption configuration settings"
   type        = any
   default     = {}
+  nullable    = false
 }
 
 variable "name" {
   description = "A friendly name of the rule group"
   type        = string
   default     = ""
+  nullable    = false
 }
 
 variable "rule_group" {
   description = "A configuration block that defines the rule group rules. Required unless `rules` is specified"
   type        = any
   default     = {}
+  nullable    = false
 }
 
 variable "rules" {
@@ -54,6 +60,7 @@ variable "type" {
   description = "Whether the rule group is stateless (containing stateless rules) or stateful (containing stateful rules). Valid values include: `STATEFUL` or `STATELESS`"
   type        = string
   default     = "STATELESS"
+  nullable    = false
 }
 
 ################################################################################
@@ -64,30 +71,35 @@ variable "create_resource_policy" {
   description = "Controls if a resource policy should be created"
   type        = bool
   default     = false
+  nullable    = false
 }
 
 variable "resource_policy_actions" {
   description = "A list of IAM actions allowed in the resource policy"
   type        = list(string)
   default     = []
+  nullable    = false
 }
 
 variable "resource_policy_principals" {
   description = "A list of IAM principals allowed in the resource policy"
   type        = list(string)
   default     = []
+  nullable    = false
 }
 
 variable "attach_resource_policy" {
   description = "Controls if a resource policy should be attached to the rule group"
   type        = bool
   default     = false
+  nullable    = false
 }
 
 variable "resource_policy" {
   description = "The policy JSON to use for the resource policy; required when `create_resource_policy` is `false`"
   type        = string
   default     = ""
+  nullable    = false
 }
 
 ################################################################################
@@ -98,4 +110,5 @@ variable "ram_resource_associations" {
   description = "A map of RAM resource associations for the created rule group"
   type        = map(string)
   default     = {}
+  nullable    = false
 }
